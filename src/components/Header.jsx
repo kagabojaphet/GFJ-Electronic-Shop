889bfeb4cd07b1f36445097a27017db1658f9dae
kagabojaphet <iradukundajaphet59@gmail.com>
2023-11-14 14:28:13 +0200

    add

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import logo from "../assests/logo.png";
+import React from "react";
+
+function Header(){
+    return(
+        <>
+        <nav>
+            <div className="logo">
+                <img src={logo} alt="" />
+            </div>
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Who We Are</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Order</a></li>
+                <li><a href="#"><i class="fa-solid fa-cart-shopping"></i>Shop</a></li>
+            </ul>
+        </nav>
+        <div className="home-container">
+            <div className="home-text">
+                <h1>GFJ-Electronic-Shop</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Deserunt, minima? Nam dolore incidunt dolorum voluptas 
+                    exercitationem, fuga eligendi perspiciatis neque hic 
+                    recusandae aut voluptatibus odio nobis eveniet, 
+                    doloribus soluta obcaecati?
+                </p>
+                <a href="">Shop Now</a>
+            </div>
+        </div>
+        <div className="about-container">
+            
+        </div>
+        </>
+    )
+}
+export default Header
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../sass/main.scss";
+import Header from "../components/Header";
+
+
+function Welcome(){
+    return(
+        <div className="welcome-container">
+            <Header/>
+        </div>
+    )
+}
+export default Welcome;
