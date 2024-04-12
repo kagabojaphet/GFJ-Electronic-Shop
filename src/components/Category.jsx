8afc2be036cd264e54009ca3817c918e6896df22
kagabojaphet <iradukundajaphet59@gmail.com>
2024-04-12 13:44:49 +0200

    add

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,9 +6,22 @@ const AllCategory=()=>{
         <div className="category-container">
             <div className="category">
                 <h1>How it Works</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum error nesciunt
+                <div className="category-pagraph">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum error nesciunt
+                     veniam repellendus repellat. Autem quia enim neque. Architecto laboriosam eligendi
+                      maxime nam neque! Non, temporibus asperiores? Debitis, minus provident 
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum error nesciunt
                      veniam repellendus repellat. Autem quia enim neque. Architecto laboriosam eligendi
                       maxime nam neque! Non, temporibus asperiores? Debitis, minus provident?</p>
+                </div>
+                <div className="category-option">
+                    <div className="option">
+                    <FontAwesomeIcon icon="fa-regular fa-location-dot" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iusto dignissimos eum odit
+                         aliquid nihil voluptatum eveniet ipsa nisi itaque repellendus, veritatis totam quidem 
+                         dolor amet illum. Provident, repudiandae consequatur?</p>
+                         </div>
+                </div>
             </div>
         </div>
         </>
--- src/components/New.jsx
+++ src/components/New.jsx
@@ -4,6 +4,7 @@ import device1 from "../assests/device1.jpeg";
 import device2 from "../assests/device2.webp";
 import device3 from "../assests/device3.jpeg";
 import device4 from "../assests/device4.jpeg";
+import device5 from "../assests/device5.webp";
 
 const NewProduct=()=>{
     return(
@@ -13,6 +14,7 @@ const NewProduct=()=>{
             <NewProps new="New" image={device2} name="Computer" title="HP new vision pro" cost="40,000"/>
             <NewProps new="New" image={device3} name="Computer" title="HP new vision pro" cost="40,000"/>
             <NewProps new="New" image={device4} name="Computer" title="HP new vision pro" cost="40,000"/>
+            <NewProps new="New" image={device5} name="Computer" title="HP new vision pro" cost="40,000"/>
         </div>
         </>
     )
