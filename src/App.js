42f3c32934331a39c0ef3a950c2caed4945c28f4
kagabojaphet <iradukundajaphet59@gmail.com>
2024-05-19 19:06:12 +0200

    add

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Welcome from './pages/Welcome';
 import { BrowserRouter as Router,Route,Routes,} from 'react-router-dom';
 import All from "./pages/All";
 import BuyPro from './pages/Buy';
-import Login from './components/login';
 import Admin from './components/Admin/Administor';
 import TableUSer from './components/Admin/UserTable';
 import Add from './components/AddToCart';
 import AddtoCartProps from './components/AddProps';
 import Product from './components/ProAdmin/PpstProduct';
+import LoginSignup from './components/Login';
 function App() {
   return (
     <Router>
@@ -17,12 +17,12 @@ function App() {
         <Route path='/' element={<Welcome/>}></Route>
         <Route path='/All' element={<All/>}></Route>
         <Route path='/Buy' element={<BuyPro/>}></Route>
-        <Route path='/Login' element={<Login/>}></Route>
         <Route path='/Admin' element={<Admin/>}></Route>
         <Route path='/Table' element={<TableUSer/>}></Route>
         <Route path='/Add' element={<Add/>}></Route>
         <Route path='/AddProps'  element={<AddtoCartProps/>}></Route>
         <Route path='/Product'  element={<Product/>}></Route>
+        <Route path='/Signup' element={<LoginSignup/>}></Route>
       </Routes>
     </Router>
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,38 @@ import React from "react";
 const LoginSignup=()=>{
     return(
         <>
-        <div className="Container"></div>
+        <div className="Container">
+        <div className="contact-form">
+              <form action="">
+                <div className="input-box">
+                  <input type="text" name=" " required="required" />
+                  <span>First Name</span>
+                </div>
+                <div className="input-box">
+                  <input type="text" name=" " required="required" />
+                  <span>Last Name</span>
+                </div>
+                <div className="input-box">
+                  <input type="text" name=" " required="required" />
+                  <span>Email</span>
+                </div>
+                <div className="input-box">
+                  <textarea
+                    name=""
+                    id=""
+                    cols="30"
+                    rows="4"
+                    required="required"
+                  ></textarea>
+                  <span>Type your Message</span>
+                </div>
+                <div className="input-box">
+                  <input type="submit" value="Send" />
+                </div>
+              </form>
+            </div>
+        </div>
         </>
     )
 }
+export default LoginSignup
